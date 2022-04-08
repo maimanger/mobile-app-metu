7d78c0319a33a3b1b694e3d0b6ba1a9961f90a2b
Xin Liu <liu.xin7@northeastern.edu>
2022-04-08 02:15:56 -0700

    Drafted profile screen with background color and card view. .

--- app/src/main/java/edu/neu/madcourse/metu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/metu/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import edu.neu.madcourse.metu.contacts.ContactsActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button profileButton = (Button) findViewById(R.id.button_profile);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onClickContacts(View view) {
