376c70db40e0a9b3994df4257e75fd2e6ca4c77a
Xin Liu <liu.xin7@northeastern.edu>
2022-04-22 03:35:46 -0700

    Added user information update API.

--- app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
@@ -15,10 +15,11 @@ import android.widget.Toast;
 import edu.neu.madcourse.metu.MainActivity;
 import edu.neu.madcourse.metu.R;
 import edu.neu.madcourse.metu.profile.imageUpload.UploadActivity;
+import edu.neu.madcourse.metu.service.FirebaseService;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    EditText etName, etEmail, etLocation, etBirthday, etGender;
+    EditText etUsername, etEmail, etLocation, etAge, etGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +36,10 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     void viewInitializations() {
-        etName = findViewById(R.id.et_name);
+        etUsername = findViewById(R.id.et_username);
         etEmail = findViewById(R.id.et_email);
         etLocation = findViewById(R.id.et_location);
-        etBirthday = findViewById(R.id.et_birthday);
+        etAge = findViewById(R.id.et_age);
         etGender = findViewById(R.id.et_gender);
 
         // To show back button in actionbar
@@ -47,8 +48,8 @@ public class EditProfileActivity extends AppCompatActivity {
 
     // Checking if the input in form is valid
     boolean validateInput() {
-        if (etName.getText().toString().equals("")) {
-            etName.setError("Please Enter Name");
+        if (etUsername.getText().toString().equals("")) {
+            etUsername.setError("Please Enter Username");
             return false;
         }
 
@@ -62,8 +63,8 @@ public class EditProfileActivity extends AppCompatActivity {
             return false;
         }
 
-        if (etBirthday.getText().toString().equals("")) {
-            etBirthday.setError("Please Enter Your Birthday");
+        if (etAge.getText().toString().equals("")) {
+            etAge.setError("Please Enter Your Age");
             return false;
         }
 
@@ -89,20 +90,17 @@ public class EditProfileActivity extends AppCompatActivity {
 
     public void performEditProfile (View v) {
         if (validateInput()) {
-
             // Input is valid, here send data to your server
-
-            String name = etName.getText().toString();
+            String username = etUsername.getText().toString();
             String email = etEmail.getText().toString();
             String location = etLocation.getText().toString();
-            String birthday = etBirthday.getText().toString();
+            String age = etAge.getText().toString();
             String gender = etGender.getText().toString();
 
+            // Write user data to firebase
+            FirebaseService.getInstance().updateUserProfile(username, email, location, age, gender);
             Toast.makeText(this,"Profile Update Successfully",Toast.LENGTH_SHORT).show();
-            // Here you can call you API
-
         }
     }
-
 }
 
---
+++ app/src/main/java/edu/neu/madcourse/metu/service/FirebaseService.java
@@ -0,0 +1,40 @@
+package edu.neu.madcourse.metu.service;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirebaseService {
+    private static FirebaseService singleton_instance = null;
+    private final DatabaseReference databaseRef;
+
+    private FirebaseService() {
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public static FirebaseService getInstance() {
+        if (singleton_instance == null) {
+            singleton_instance = new FirebaseService();
+        }
+        return singleton_instance;
+    }
+
+    public void updateUserProfile(String username, String email, String location, String age,
+                                  String gender) {
+        // Firebase does not allow '.' character in key.
+        String key = email.replace(".", "DOT");
+        HashMap<String, Object> userValues = new HashMap<>();
+        userValues.put("username", username);
+        userValues.put("email", email);
+        userValues.put("location", location);
+        userValues.put("age", age);
+        userValues.put("gender", gender);
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/users/" + key, userValues);
+
+        databaseRef.updateChildren(childUpdates);
+    }
+}
