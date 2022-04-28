819c01a487ed062639fa48f865313793fb859052
Xin Liu <liu.xin7@northeastern.edu>
2022-04-28 04:35:15 -0700

    Added gender spinner.

--- app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
@@ -8,23 +8,34 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import edu.neu.madcourse.metu.App;
 import edu.neu.madcourse.metu.BaseCalleeActivity;
@@ -43,7 +54,8 @@ public class EditProfileActivity extends BaseCalleeActivity {
     private Bitmap avatarBitmap;
     private ActivityResultLauncher<Intent> uploadActivityResultLauncher;
     private String profileUserId;
-    ImageView uploadImageView;
+    private ImageView uploadImageView;
+    private String gender;
 
 
     @Override
@@ -55,7 +67,7 @@ public class EditProfileActivity extends BaseCalleeActivity {
         viewInitializations();
 
         uploadImageView = findViewById(R.id.edit_profile_image);
-        String loginUserAvatarUri = ((App)getApplication()).getLoginUser().getAvatarUri();
+        String loginUserAvatarUri = ((App) getApplication()).getLoginUser().getAvatarUri();
         Utils.loadImgUri(loginUserAvatarUri, uploadImageView, new Callback() {
             @Override
             public void onSuccess() {
@@ -76,9 +88,11 @@ public class EditProfileActivity extends BaseCalleeActivity {
                         imageFirebaseUri = Uri.parse(data.getStringExtra("imageFirebaseUri"));
 
                         // TODO: Should be implemented in Button(R.id.bt_register) onclickListener
-                        FirebaseService.getInstance().updateUserAvatar(profileUserId, imageFirebaseUri.toString());
+                        FirebaseService.getInstance().updateUserAvatar(profileUserId,
+                                imageFirebaseUri.toString());
                         try {
-                            avatarBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageFilePath);
+                            avatarBitmap = MediaStore.Images.Media.getBitmap(getContentResolver()
+                                    , imageFilePath);
                             uploadImageView.setImageBitmap(avatarBitmap);
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -103,20 +117,53 @@ public class EditProfileActivity extends BaseCalleeActivity {
         etNickname = findViewById(R.id.et_username);
         etLocation = findViewById(R.id.et_location);
         etAge = findViewById(R.id.et_age);
-        etGender = findViewById(R.id.et_gender);
         ImageView uploadImageView = findViewById(R.id.edit_profile_image);
 
+        Spinner spinner = findViewById(R.id.gender_spinner);
 
+        String[] genders = getResources().getStringArray(R.array.genders);
 
-        FirebaseService.getInstance().fetchUserProfileData(profileUserId, user -> {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, genders) {
+            @Override
+            public boolean isEnabled(int position) {
+                return position != 0;
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView,
+                                        @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                tv.setTextColor((position == 0) ? Color.GRAY : Color.BLACK);
+                return view;
+            }
+        };
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position > 0) {
+                    gender = (String) parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        FirebaseService.getInstance().fetchUserProfileDataOneTime(profileUserId, user -> {
             etNickname.setText(user.getNickname());
             etLocation.setText(user.getLocation());
             etAge.setText(String.valueOf(user.getAge()));
-            etGender.setText(GENDER_MAP.get(user.getGender()));
+            spinner.setSelection(user.getGender() + 1);
+
             new Utils.DownloadImageTask(uploadImageView).execute(user.getAvatarUri());
         });
     }
-
 
 
     // Checking if the input in form is valid
@@ -136,8 +183,8 @@ public class EditProfileActivity extends BaseCalleeActivity {
             return false;
         }
 
-        if (etGender.getText().toString().equals("")) {
-            etGender.setError("Please Enter Gender ");
+        if (gender == null) {
+            Toast.makeText(getApplicationContext(), "Please select a gender", Toast.LENGTH_SHORT).show();
             return false;
         }
 
@@ -149,14 +196,12 @@ public class EditProfileActivity extends BaseCalleeActivity {
     }
 
     // Hook Click Event
-
     public void performEditProfile(View v) {
         if (validateInput()) {
             // Input is valid, here send data to your server
             String nickname = etNickname.getText().toString();
             String location = etLocation.getText().toString();
             Integer age = Integer.parseInt(etAge.getText().toString());
-            String gender = etGender.getText().toString();
 
             Integer genderInt = 2;
             if (gender.toLowerCase().equals("male")) {
@@ -167,7 +212,6 @@ public class EditProfileActivity extends BaseCalleeActivity {
 
             // Write user data to firebase
             User loginUser = ((App) getApplication()).getLoginUser();
-            Log.e(TAG, loginUser.toString());
             loginUser.setNickname(nickname);
             loginUser.setLocation(location);
             loginUser.setAge(age);
