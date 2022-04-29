b29af8ed69fbc1411fb9c7d24363a576ff12a1de
Xin Liu <liu.xin7@northeastern.edu>
2022-04-29 04:46:04 -0700

    Added state and city picker.

--- app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
+++ app/src/main/java/edu/neu/madcourse/metu/profile/EditProfileActivity.java
@@ -27,21 +27,28 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 
+import org.json.JSONException;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import edu.neu.madcourse.metu.App;
 import edu.neu.madcourse.metu.BaseCalleeActivity;
 import edu.neu.madcourse.metu.R;
 import edu.neu.madcourse.metu.models.User;
 import edu.neu.madcourse.metu.profile.imageUpload.UploadActivity;
+import edu.neu.madcourse.metu.service.CountryStateCityService;
 import edu.neu.madcourse.metu.service.FirebaseService;
 import edu.neu.madcourse.metu.utils.Utils;
 
 public class EditProfileActivity extends BaseCalleeActivity {
 
-    EditText etNickname, etLocation, etAge;
-    Spinner genderSpinner;
-    NumberPicker statePicker, cityPicker;
+    private EditText etNickname, etAge;
+    private Spinner genderSpinner;
+    private NumberPicker statePicker, cityPicker;
     private Uri imageFilePath;
     private Uri imageFirebaseUri;
     private Bitmap avatarBitmap;
@@ -49,6 +56,8 @@ public class EditProfileActivity extends BaseCalleeActivity {
     private String profileUserId;
     private ImageView uploadImageView;
     private String gender;
+    private String locationState, locationCity;
+    Lock cityPickerLock = new ReentrantLock();
 
 
     @Override
@@ -57,7 +66,11 @@ public class EditProfileActivity extends BaseCalleeActivity {
         setContentView(R.layout.activity_edit_profile);
 
         profileUserId = getIntent().getExtras().getString("userId");
-        viewInitializations();
+        try {
+            viewInitializations();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         uploadImageView = findViewById(R.id.edit_profile_image);
         String loginUserAvatarUri = ((App) getApplication()).getLoginUser().getAvatarUri();
@@ -81,16 +94,17 @@ public class EditProfileActivity extends BaseCalleeActivity {
                         imageFirebaseUri = Uri.parse(data.getStringExtra("imageFirebaseUri"));
 
                         runOnUiThread(() -> {
-                            Utils.loadImgUri(imageFirebaseUri.toString(), uploadImageView, new Callback() {
-                                @Override
-                                public void onSuccess() {
-                                }
+                            Utils.loadImgUri(imageFirebaseUri.toString(), uploadImageView,
+                                    new Callback() {
+                                        @Override
+                                        public void onSuccess() {
+                                        }
 
-                                @Override
-                                public void onError(Exception e) {
-                                    uploadImageView.setImageResource(R.drawable.user_avatar);
-                                }
-                            });
+                                        @Override
+                                        public void onError(Exception e) {
+                                            uploadImageView.setImageResource(R.drawable.user_avatar);
+                                        }
+                                    });
                         });
 
                         // TODO: Should be implemented in Button(R.id.bt_register) onclickListener
@@ -118,21 +132,18 @@ public class EditProfileActivity extends BaseCalleeActivity {
 
     }
 
-    void viewInitializations() {
+    void viewInitializations() throws JSONException {
         etNickname = findViewById(R.id.et_username);
-//        etLocation = findViewById(R.id.et_location);
         etAge = findViewById(R.id.et_age);
         ImageView uploadImageView = findViewById(R.id.edit_profile_image);
 
         findViewById(R.id.btn_editProfile_back).setOnClickListener(View -> onBackPressed());
 
         initStatePicker();
-//        initCityPicker();
         initGenderSpinner();
 
         FirebaseService.getInstance().fetchUserProfileDataOneTime(profileUserId, user -> {
             etNickname.setText(user.getNickname());
-//            etLocation.setText(user.getLocation());
             etAge.setText(String.valueOf(user.getAge()));
             genderSpinner.setSelection(user.getGender() + 1);
 
@@ -178,7 +189,72 @@ public class EditProfileActivity extends BaseCalleeActivity {
     }
 
     private void initStatePicker() {
-        statePicker = findViewById(R.id.state_picker);
+        new Thread(() -> {
+            statePicker = findViewById(R.id.state_picker);
+            List<String> states = null;
+            try {
+                states = CountryStateCityService.getStates();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            assert states != null;
+
+            Log.e(TAG, "states: " + states.toString());
+            final String[] statesArray = states.toArray(new String[states.size()]);
+            statePicker.setMinValue(0);
+            statePicker.setMaxValue(states.size() - 1);
+            statePicker.setDisplayedValues(statesArray);
+            statePicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+            statePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                    if (newVal >= 0 && newVal < statesArray.length) {
+                        locationState = statesArray[newVal];
+                        initCityPicker(locationState);
+                    }
+                }
+            });
+        }).start();
+    }
+
+    private void initCityPicker(String state) {
+        new Thread(() -> {
+            cityPickerLock.lock();
+
+            List<String> cities = null;
+            try {
+                cities = CountryStateCityService.getCities(state);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            assert cities != null;
+            Log.e(TAG, "cities: " + cities.toString());
+
+            if (!cities.isEmpty()) {
+                cityPicker = findViewById(R.id.city_picker);
+                final String[] citiesArray = cities.toArray(new String[cities.size()]);
+                Log.e(TAG, "citiesArray = " + Arrays.toString(citiesArray));
+
+
+                cityPicker.setDisplayedValues(null);
+                cityPicker.setMinValue(0);
+                cityPicker.setMaxValue(Math.max(cities.size() - 1, 0));
+                cityPicker.setDisplayedValues(citiesArray);
+
+                cityPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+                cityPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
+                    if (newVal >= 0 && newVal < citiesArray.length) {
+                        locationCity = citiesArray[newVal];
+                    }
+                });
+            } else {
+                cityPicker.setDisplayedValues(null);
+                cityPicker.setMinValue(0);
+                cityPicker.setMaxValue(0);
+            }
+
+            cityPickerLock.unlock();
+        }).start();
     }
 
 
@@ -188,11 +264,6 @@ public class EditProfileActivity extends BaseCalleeActivity {
             etNickname.setError("Please Enter Username");
             return false;
         }
-
-        /*if (etLocation.getText().toString().equals("")) {
-            etLocation.setError("Please Enter Location");
-            return false;
-        }*/
 
         if (etAge.getText().toString().equals("")) {
             etAge.setError("Please Enter Your Age");
@@ -216,7 +287,6 @@ public class EditProfileActivity extends BaseCalleeActivity {
         if (validateInput()) {
             // Input is valid, here send data to your server
             String nickname = etNickname.getText().toString();
-//            String location = etLocation.getText().toString();
             Integer age = Integer.parseInt(etAge.getText().toString());
 
             Integer genderInt = 2;
@@ -229,7 +299,7 @@ public class EditProfileActivity extends BaseCalleeActivity {
             // Write user data to firebase
             User loginUser = ((App) getApplication()).getLoginUser();
             loginUser.setNickname(nickname);
-//            loginUser.setLocation(location);
+            loginUser.setLocation(locationCity + ", " + locationState);
             loginUser.setAge(age);
             loginUser.setGender(genderInt);
 
--- app/src/main/java/edu/neu/madcourse/metu/service/CountryStateCityService.java
+++ app/src/main/java/edu/neu/madcourse/metu/service/CountryStateCityService.java
@@ -2,12 +2,17 @@ package edu.neu.madcourse.metu.service;
 
 import android.util.Log;
 
+import org.apache.http.HttpConnection;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,55 +22,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CountryStateCityService {
     private static final String TAG = "CountryStateCityService";
-    private final String USER_EMAIL = "alice@bob.com";
-    private final String API_TOKEN = "WxKOzHVWlBswYGXt1zoDZFjyQpYtAHsICAS9Aal0zxQmvbofAF_wtXMZr0oyTsq-ZNk";
-    private final String BASE_ROUTE = "https://www.universal-tutorial.com/api/";
+    private static final String USER_EMAIL = "alice@bob.com";
+    private static final String API_TOKEN =
+            "WxKOzHVWlBswYGXt1zoDZFjyQpYtAHsICAS9Aal0zxQmvbofAF_wtXMZr0oyTsq-ZNk";
+    private static final String BASE_ROUTE = "https://www.universal-tutorial.com/api/";
 
-    public String getAuthToken() throws JSONException {
-        HttpClient httpclient = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(BASE_ROUTE + "getaccesstoken");
-        request.setHeader("api-token", API_TOKEN);
-        request.setHeader("user-email", USER_EMAIL);
-        request.addHeader("Content-Type", "application/json");
+    private static CountryStateCityService singleton_instance = null;
 
+    private CountryStateCityService() {
+    }
+
+    public static CountryStateCityService getInstance() {
+        if (singleton_instance == null) {
+            singleton_instance = new CountryStateCityService();
+        }
+        return singleton_instance;
+    }
+
+    private static String getAuthToken() throws JSONException {
         String auth_token = "";
         try {
-            HttpResponse response = httpclient.execute(request);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == HttpStatus.SC_OK) {
-                String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-                JSONObject auth_token_json = new JSONObject(responseBody);
-                auth_token = auth_token_json.getString("auth_token");
-            } else {
-                Log.e(TAG, "HTTP GET returned " + statusCode);
-            }
-        } catch (IOException e) {
+            String jsonStr = readJsonStrFromUrl(BASE_ROUTE + "getaccesstoken",
+                    "", new HashMap<String, String>() {{
+                        put("Accept", "application/json");
+                        put("api-token", API_TOKEN);
+                        put("user-email", USER_EMAIL);
+                    }});
+            auth_token = (new JSONObject(jsonStr)).getString("auth_token");
+//            Log.e(TAG, "auth token = " + auth_token);
+        } catch (JSONException | IOException e) {
+            Log.e(TAG, "Failed to get auth token");
             e.printStackTrace();
-        } finally {
-            request.releaseConnection();
         }
 
         return auth_token;
     }
 
-    public List<String> getStates() throws JSONException {
+    public static List<String> getStates() throws JSONException {
         String country = "United States";
         JSONArray stateArray = null;
         try {
-            JSONObject json = readJsonFromUrl(BASE_ROUTE + "states/" + country);
-            stateArray = new JSONArray(json);
-        } catch (JSONException | IOException e) {
-            Log.e(TAG, "Failed to get list of states: " + Arrays.toString(e.getStackTrace()));
+            String jsonStr = readJsonStrFromUrl(BASE_ROUTE + "states/" + country,
+                    "Bearer " + getAuthToken(), new HashMap<String, String>() {{
+                        put("Accept", "application/json");
+                    }});
+            stateArray = new JSONArray(jsonStr);
+        } catch (JSONException e) {
+            Log.e(TAG, "JSONException: Failed to get list of states");
+            e.printStackTrace();
+            return new ArrayList<>();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: Failed to get list of states");
+            e.printStackTrace();
             return new ArrayList<>();
         }
 
@@ -78,36 +102,72 @@ public class CountryStateCityService {
         return states;
     }
 
-    public List<String> getCities(String state) throws JSONException {
+    public static List<String> getCities(String state) throws JSONException {
         JSONArray cityArray = null;
         try {
-            JSONObject json = readJsonFromUrl(BASE_ROUTE + "cities/" + state);
-            cityArray = new JSONArray(json);
+            String jsonStr = readJsonStrFromUrl(BASE_ROUTE + "cities/" + state,
+                    "Bearer " + getAuthToken(), new HashMap<String, String>() {{
+                        put("Accept", "application/json");
+                    }});
+            cityArray = new JSONArray(jsonStr);
         } catch (IOException | JSONException e) {
-            Log.e(TAG, "Failed to get list of cities: " + Arrays.toString(e.getStackTrace()));
+            Log.e(TAG, "Failed to get list of cities");
+            e.printStackTrace();
             return new ArrayList<>();
         }
 
-        List<String> states = new ArrayList<>();
+        List<String> cities = new ArrayList<>();
         for (int i = 0; i < cityArray.length(); ++i) {
             JSONObject record = cityArray.getJSONObject(i);
             String city = record.getString("city_name");
-            states.add(state);
+            cities.add(city);
         }
-        return states;
+        return cities;
 
     }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is,
-                    StandardCharsets.UTF_8));
-            String jsonText = readAll(rd);
-            return new JSONObject(jsonText);
-        } catch (IOException e) {
-            e.getStackTrace();
-            return null;
+    public static String readJsonStrFromUrl(String url, String authToken,
+                                            Map<String, String> headerMap) throws IOException,
+            JSONException {
+        Log.e(TAG, "url: " + url);
+        URL webUrl = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) webUrl.openConnection();
+        conn.setUseCaches(true);
+        conn.setDoInput(true);
+
+        if (authToken != null && !authToken.isEmpty()) {
+            conn.setRequestProperty("Authorization", authToken);
         }
+
+        for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+            conn.setRequestProperty(entry.getKey(), entry.getValue());
+        }
+
+        conn.setRequestMethod("GET");
+//        Log.e(TAG, conn.getRequestProperties().toString());
+
+        int respCode = conn.getResponseCode(); // New items get NOT_FOUND on PUT
+//        Log.e(TAG, "response code: " + respCode);
+
+        if (respCode == HttpURLConnection.HTTP_OK || respCode == HttpURLConnection.HTTP_NOT_FOUND) {
+            InputStream is = conn.getInputStream();
+
+            try {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+                        StandardCharsets.UTF_8));
+                return readAll(rd);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "MalformedURLException");
+                e.getStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException");
+                e.getStackTrace();
+            } finally {
+                conn.disconnect();
+                is.close();
+            }
+        }
+        return "";
     }
 
     private static String readAll(Reader rd) throws IOException {
